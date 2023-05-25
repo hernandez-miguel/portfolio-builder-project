dc7c7710b2f46b238aa5672ab995feab80d048ed
Miguel Hernandez <hernandez.miguel95@yahoo.com>
2023-05-25 16:08:48 -0500

    finished making table responsive

--- src/components/TableBodyData.jsx
+++ src/components/TableBodyData.jsx
@@ -1,15 +1,12 @@
 function TableBodyData() {
   const dataCellNames = [
-    'target weight',
-    'ticker symbol',
-    'stock name',
-    'payout months',
-    'dividend yield',
-    'weighted div yield',
-    '5y cagr',
-    '5y dividend growth',
-    'weighted cagr',
-    'weighted dividend growth',
+    '(%) Allocation',
+    'Ticker Symbol',
+    'Stock Name',
+    'Payout Months',
+    'Dividend Yield',
+    '5y CAGR',
+    '5y Dividend growth',
   ];
 
   let counter = 0;
@@ -19,21 +16,20 @@ function TableBodyData() {
     <>
       {dataCellNames.map((row) => {
         return (
-        <tr key = {rowCounter++}>
+        <tr key={rowCounter++}>
           {dataCellNames.map((item) => {
             counter++;
-            console.log('counter:', counter)
             if (counter < 3) {
               return (
-                <td key={item}>
+                <td key={item} data-cell={item}>
                   <input type="text" />
                 </td>
               );
             }
-            if (counter === 10) {
+            if (counter === 7) {
               counter = 0;
             }
-            return <td key={item}>test</td>;
+            return <td key={item} data-cell={item}>-</td>
           })}
         </tr> )
       })}
--- src/pages/table.pages.jsx
+++ src/pages/table.pages.jsx
@@ -1,32 +1,32 @@
 import { useState } from 'react';
 import TableBodyData from '../components/TableBodyData';
+import './style.css';
 
 function TablePage() {
   return (
     <>
-      <h1>Portfolio Builder</h1>
-      <form action="">
-        <table>
-          <caption>Stock Portfolio</caption>
-          <thead>
-            <tr>
-              <th>Target Weight(%)</th>
-              <th>Ticker Symbol</th>
-              <th>Stock Name</th>
-              <th>Payout Months</th>
-              <th>Dividend Yield</th>
-              <th>Weighted Div Yield</th>
-              <th>5Y CAGR</th>
-              <th>5Y Dividend Growth</th>
-              <th>Weighted CAGR</th>
-              <th>Weighted Dividend Growth</th>
-            </tr>
-          </thead>
-          <tbody>
-            <TableBodyData />
-          </tbody>
-        </table>
-      </form>
+      <h1>Responsive Tables</h1>
+      <div className="table-container">
+        <form action="">
+          <table>
+            <caption>Stock Portfolio Builder</caption>
+            <thead>
+              <tr>
+                <th>(%) Allocation</th>
+                <th>Ticker Symbol</th>
+                <th>Stock Name</th>
+                <th>Payout Months</th>
+                <th>Dividend Yield</th>
+                <th>5Y CAGR</th>
+                <th>5Y Dividend Growth</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TableBodyData />
+            </tbody>
+          </table>
+        </form>
+      </div>
     </>
   );
 }
